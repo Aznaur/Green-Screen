205b2fcd415e2a59f60bee074e9dcead33f5fcc5
Aznaur <aznaur.khapchaev@gmail.com>
2022-08-09 16:48:49 +0300

    First commit

---
+++ config/path.js
@@ -0,0 +1,36 @@
+
+export const path =  {
+    src: {
+        base: 'src/',
+        html: 'src/*.html',
+        css: 'src/css/',
+        scss: 'src/scss/**/*.scss',
+        styleScss: 'src/scss/style.scss',
+        js: 'src/js/index.js',
+        img: 'src/img/**/*.{jpg,png,jpeg,webp,svg}',
+        webp: 'src/img/**/*.{jpg,png,jpeg,webp}',
+        fonts: 'src/fonts/',
+        rigger: 'src/html/*.html',
+        riggerWatch: 'src/html/**/*.html',
+        assets: ['src/fonts/**/*.ttf', 'src/icons/**/*.*']
+    },
+
+    dist: {
+        base: 'build/',
+        html: 'build/',
+        css: 'build/css/',
+        js: 'build/js/',
+        img: 'build/img/',
+        fonts: 'build/fonts/'
+    },
+
+    watch: {
+        base: 'src/',
+        html: 'src/*.html',
+        scss: '/scss/**/*.*',
+        js: 'src/js/**/*.js',
+        img: 'src/img/**/*.{jpg,png,jpeg,svg}',
+    },
+
+    dev: false
+}
---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+import gulp from 'gulp';
+
+// Конфигурация
+import { path } from './config/path.js';
+import { plugins } from './config/plugins.js';
+// let dev = false;
+
+//Задачи
+import { clear } from './task/clear.js';
+import { html } from './task/html.js';
+import { css } from './task/css.js';
+import { server } from './task/server.js';
+import { images } from './task/image.js';
+import { webp } from './task/webp.js';
+import { copy } from './task/copy.js';
+import { fonts } from './task/fonts.js';
+import { rigger } from './task/rigger.js';
+
+// Глобальная переменная
+global.app = {
+    gulp: gulp,
+    plugins: plugins,
+    path: path
+}
+
+// Наблюдатель
+const watcher = () => {
+    gulp.watch(app.path.src.html).on("change", app.plugins.bs.reload);
+    gulp.watch(app.path.src.riggerWatch, rigger);
+    gulp.watch(app.path.src.scss, css);
+}
+
+const develop = (ready) => {
+    app.path.dev = true;
+    ready();
+}
+
+
+//Разовые задачи
+export { clear };
+export { images};
+export { webp };
+export { copy };
+export { fonts };
+export { rigger };
+
+
+export const base = gulp.parallel(css, rigger);
+export const build = gulp.series(clear, html, base);
+export default gulp.series(develop, base, gulp.parallel(watcher, server));
